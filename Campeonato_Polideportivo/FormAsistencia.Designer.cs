8382f16408421feeb23b7bce406d9da89f0bd920
gabriela-55 <sucgabi@gmail.com>
2024-08-02 20:08:51 -0600

    Agregación de Form Asistencia

--- Campeonato_Polideportivo/FormAsistencia.Designer.cs
+++ Campeonato_Polideportivo/FormAsistencia.Designer.cs
@@ -33,10 +33,15 @@ namespace Campeonato_Polideportivo
             this.BtnModificar = new System.Windows.Forms.Button();
             this.BtnIngresar = new System.Windows.Forms.Button();
             this.CmbEquipo = new System.Windows.Forms.ComboBox();
-            this.CmbDeporte = new System.Windows.Forms.ComboBox();
+            this.CmbJugador = new System.Windows.Forms.ComboBox();
             this.LblAsistencia = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.DgvAsistencia = new System.Windows.Forms.DataGridView();
+            this.BtnVer = new System.Windows.Forms.Button();
+            this.TxtIdAsistencia = new System.Windows.Forms.TextBox();
+            this.LblIdAsistencia = new System.Windows.Forms.Label();
+            this.TxtMinuto = new System.Windows.Forms.TextBox();
+            this.LblMinuto = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvAsistencia)).BeginInit();
             this.SuspendLayout();
             // 
             // BtnEliminar
@@ -49,12 +54,14 @@ namespace Campeonato_Polideportivo
             this.BtnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnEliminar.Font = new System.Drawing.Font("Haettenschweiler", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnEliminar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
-            this.BtnEliminar.Location = new System.Drawing.Point(668, 540);
+            this.BtnEliminar.Location = new System.Drawing.Point(574, 470);
+            this.BtnEliminar.Margin = new System.Windows.Forms.Padding(2);
             this.BtnEliminar.Name = "BtnEliminar";
-            this.BtnEliminar.Size = new System.Drawing.Size(174, 41);
+            this.BtnEliminar.Size = new System.Drawing.Size(130, 33);
             this.BtnEliminar.TabIndex = 55;
             this.BtnEliminar.Text = "ELIMINAR";
             this.BtnEliminar.UseVisualStyleBackColor = false;
+            this.BtnEliminar.Click += new System.EventHandler(this.BtnEliminar_Click);
             // 
             // BtnModificar
             // 
@@ -66,12 +73,14 @@ namespace Campeonato_Polideportivo
             this.BtnModificar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnModificar.Font = new System.Drawing.Font("Haettenschweiler", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnModificar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
-            this.BtnModificar.Location = new System.Drawing.Point(408, 540);
+            this.BtnModificar.Location = new System.Drawing.Point(409, 470);
+            this.BtnModificar.Margin = new System.Windows.Forms.Padding(2);
             this.BtnModificar.Name = "BtnModificar";
-            this.BtnModificar.Size = new System.Drawing.Size(174, 41);
+            this.BtnModificar.Size = new System.Drawing.Size(130, 33);
             this.BtnModificar.TabIndex = 54;
             this.BtnModificar.Text = "MODIFICAR";
             this.BtnModificar.UseVisualStyleBackColor = false;
+            this.BtnModificar.Click += new System.EventHandler(this.BtnModificar_Click);
             // 
             // BtnIngresar
             // 
@@ -83,72 +92,142 @@ namespace Campeonato_Polideportivo
             this.BtnIngresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnIngresar.Font = new System.Drawing.Font("Haettenschweiler", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnIngresar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
-            this.BtnIngresar.Location = new System.Drawing.Point(153, 540);
+            this.BtnIngresar.Location = new System.Drawing.Point(252, 470);
+            this.BtnIngresar.Margin = new System.Windows.Forms.Padding(2);
             this.BtnIngresar.Name = "BtnIngresar";
-            this.BtnIngresar.Size = new System.Drawing.Size(174, 41);
+            this.BtnIngresar.Size = new System.Drawing.Size(130, 33);
             this.BtnIngresar.TabIndex = 53;
             this.BtnIngresar.Text = "INGRESAR";
             this.BtnIngresar.UseVisualStyleBackColor = false;
+            this.BtnIngresar.Click += new System.EventHandler(this.BtnIngresar_Click);
             // 
             // CmbEquipo
             // 
-            this.CmbEquipo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.CmbEquipo.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.CmbEquipo.FormattingEnabled = true;
-            this.CmbEquipo.Location = new System.Drawing.Point(304, 173);
+            this.CmbEquipo.Location = new System.Drawing.Point(252, 106);
+            this.CmbEquipo.Margin = new System.Windows.Forms.Padding(2);
             this.CmbEquipo.Name = "CmbEquipo";
-            this.CmbEquipo.Size = new System.Drawing.Size(381, 24);
+            this.CmbEquipo.Size = new System.Drawing.Size(287, 21);
             this.CmbEquipo.TabIndex = 52;
             this.CmbEquipo.Text = "Selecciona un equipo...";
+            this.CmbEquipo.SelectedIndexChanged += new System.EventHandler(this.CmbEquipo_SelectedIndexChanged);
             // 
-            // CmbDeporte
+            // CmbJugador
             // 
-            this.CmbDeporte.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
-            this.CmbDeporte.FormattingEnabled = true;
-            this.CmbDeporte.Location = new System.Drawing.Point(304, 143);
-            this.CmbDeporte.Name = "CmbDeporte";
-            this.CmbDeporte.Size = new System.Drawing.Size(381, 24);
-            this.CmbDeporte.TabIndex = 51;
-            this.CmbDeporte.Text = "Selecciona un deporte...";
+            this.CmbJugador.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.CmbJugador.FormattingEnabled = true;
+            this.CmbJugador.Location = new System.Drawing.Point(252, 148);
+            this.CmbJugador.Margin = new System.Windows.Forms.Padding(2);
+            this.CmbJugador.Name = "CmbJugador";
+            this.CmbJugador.Size = new System.Drawing.Size(287, 21);
+            this.CmbJugador.TabIndex = 51;
+            this.CmbJugador.Text = "Selecciona un jugador...";
             // 
             // LblAsistencia
             // 
-            this.LblAsistencia.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.LblAsistencia.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.LblAsistencia.AutoSize = true;
             this.LblAsistencia.Font = new System.Drawing.Font("Haettenschweiler", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblAsistencia.Location = new System.Drawing.Point(434, 71);
+            this.LblAsistencia.Location = new System.Drawing.Point(297, 42);
+            this.LblAsistencia.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.LblAsistencia.Name = "LblAsistencia";
-            this.LblAsistencia.Size = new System.Drawing.Size(199, 50);
+            this.LblAsistencia.Size = new System.Drawing.Size(160, 40);
             this.LblAsistencia.TabIndex = 50;
             this.LblAsistencia.Text = "ASISTENCIA";
             // 
-            // dataGridView1
+            // DgvAsistencia
             // 
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(109, 225);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(776, 293);
-            this.dataGridView1.TabIndex = 49;
+            this.DgvAsistencia.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.DgvAsistencia.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DgvAsistencia.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvAsistencia.Location = new System.Drawing.Point(252, 188);
+            this.DgvAsistencia.Margin = new System.Windows.Forms.Padding(2);
+            this.DgvAsistencia.Name = "DgvAsistencia";
+            this.DgvAsistencia.RowHeadersWidth = 51;
+            this.DgvAsistencia.RowTemplate.Height = 24;
+            this.DgvAsistencia.Size = new System.Drawing.Size(452, 168);
+            this.DgvAsistencia.TabIndex = 49;
+            this.DgvAsistencia.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgvAsistencia_CellContentClick);
+            // 
+            // BtnVer
+            // 
+            this.BtnVer.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.BtnVer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(63)))), ((int)(((byte)(63)))));
+            this.BtnVer.FlatAppearance.BorderSize = 0;
+            this.BtnVer.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.BtnVer.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.BtnVer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnVer.Font = new System.Drawing.Font("Haettenschweiler", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnVer.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.BtnVer.Location = new System.Drawing.Point(102, 470);
+            this.BtnVer.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnVer.Name = "BtnVer";
+            this.BtnVer.Size = new System.Drawing.Size(130, 33);
+            this.BtnVer.TabIndex = 56;
+            this.BtnVer.Text = "VER";
+            this.BtnVer.UseVisualStyleBackColor = false;
+            this.BtnVer.Click += new System.EventHandler(this.BtnVer_Click);
+            // 
+            // TxtIdAsistencia
+            // 
+            this.TxtIdAsistencia.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.TxtIdAsistencia.Location = new System.Drawing.Point(132, 234);
+            this.TxtIdAsistencia.Name = "TxtIdAsistencia";
+            this.TxtIdAsistencia.Size = new System.Drawing.Size(100, 20);
+            this.TxtIdAsistencia.TabIndex = 57;
+            this.TxtIdAsistencia.TextChanged += new System.EventHandler(this.TxtIdAsistencia_TextChanged);
+            // 
+            // LblIdAsistencia
+            // 
+            this.LblIdAsistencia.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.LblIdAsistencia.AutoSize = true;
+            this.LblIdAsistencia.Location = new System.Drawing.Point(31, 234);
+            this.LblIdAsistencia.Name = "LblIdAsistencia";
+            this.LblIdAsistencia.Size = new System.Drawing.Size(68, 13);
+            this.LblIdAsistencia.TabIndex = 58;
+            this.LblIdAsistencia.Text = "ID asistencia";
+            // 
+            // TxtMinuto
+            // 
+            this.TxtMinuto.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.TxtMinuto.Location = new System.Drawing.Point(132, 301);
+            this.TxtMinuto.Name = "TxtMinuto";
+            this.TxtMinuto.Size = new System.Drawing.Size(100, 20);
+            this.TxtMinuto.TabIndex = 59;
+            // 
+            // LblMinuto
+            // 
+            this.LblMinuto.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.LblMinuto.AutoSize = true;
+            this.LblMinuto.Location = new System.Drawing.Point(31, 304);
+            this.LblMinuto.Name = "LblMinuto";
+            this.LblMinuto.Size = new System.Drawing.Size(39, 13);
+            this.LblMinuto.TabIndex = 60;
+            this.LblMinuto.Text = "Minuto";
             // 
             // FormAsistencia
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(995, 653);
+            this.ClientSize = new System.Drawing.Size(746, 531);
+            this.Controls.Add(this.LblMinuto);
+            this.Controls.Add(this.TxtMinuto);
+            this.Controls.Add(this.LblIdAsistencia);
+            this.Controls.Add(this.TxtIdAsistencia);
+            this.Controls.Add(this.BtnVer);
             this.Controls.Add(this.BtnEliminar);
             this.Controls.Add(this.BtnModificar);
             this.Controls.Add(this.BtnIngresar);
             this.Controls.Add(this.CmbEquipo);
-            this.Controls.Add(this.CmbDeporte);
+            this.Controls.Add(this.CmbJugador);
             this.Controls.Add(this.LblAsistencia);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.DgvAsistencia);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FormAsistencia";
             this.Text = "FormAsistencia";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Load += new System.EventHandler(this.FormAsistencia_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DgvAsistencia)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -160,8 +239,13 @@ namespace Campeonato_Polideportivo
         private System.Windows.Forms.Button BtnModificar;
         private System.Windows.Forms.Button BtnIngresar;
         private System.Windows.Forms.ComboBox CmbEquipo;
-        private System.Windows.Forms.ComboBox CmbDeporte;
+        private System.Windows.Forms.ComboBox CmbJugador;
         private System.Windows.Forms.Label LblAsistencia;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView DgvAsistencia;
+        private System.Windows.Forms.Button BtnVer;
+        private System.Windows.Forms.TextBox TxtIdAsistencia;
+        private System.Windows.Forms.Label LblIdAsistencia;
+        private System.Windows.Forms.TextBox TxtMinuto;
+        private System.Windows.Forms.Label LblMinuto;
     }
 }
--- Campeonato_Polideportivo/FormAsistencia.cs
+++ Campeonato_Polideportivo/FormAsistencia.cs
@@ -8,13 +8,303 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.IO;
+using MySql.Data.MySqlClient;
+
 namespace Campeonato_Polideportivo
 {
     public partial class FormAsistencia : Form
     {
+
+
         public FormAsistencia()
         {
             InitializeComponent();
+
+            CargarEquipos();
+
+        }
+
+        private void CargarEquipos()
+        {
+            
+            Conexion conexion = new Conexion();
+            MySqlConnection conn = conexion.getConexion();
+
+            string query = "SELECT pkidequipo, nombre FROM equipo";
+
+            try
+            {
+                MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+
+                CmbEquipo.DataSource = dt;
+                CmbEquipo.DisplayMember = "nombre";
+                CmbEquipo.ValueMember = "pkidequipo"; // Eliminar espacio adicional
+
+                CmbEquipo.SelectedIndexChanged += new EventHandler(CmbEquipo_SelectedIndexChanged);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+
+        private void BtnVer_Click(object sender, EventArgs e)
+        {
+            int jugadorId;
+            if (!int.TryParse(TxtIdAsistencia.Text, out jugadorId))
+            {
+                MessageBox.Show("ID inválido.");
+                return;
+            }
+
+            Conexion conexion = new Conexion();
+            MySqlConnection conn = conexion.getConexion();
+
+            //string query = "SELECT pkidasistencia, minuto FROM asistencia WHERE fkidjugador = @fkidjugador";
+            //string query = "SELECT pkidasistencia, minuto, fkidjugador FROM asistencia WHERE pkidasistencia = @pkidasistencia";
+            string query = "SELECT a.pkidasistencia, a.minuto, j.apellido " +
+                   "FROM asistencia a " +
+                   "JOIN jugador j ON a.fkidjugador = j.pkidjugador " +
+                   "WHERE a.pkidasistencia = @pkidasistencia";
+
+            try
+            {
+                MySqlCommand command = new MySqlCommand(query, conn);
+                command.Parameters.AddWithValue("@pkidasistencia", jugadorId);
+
+                MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+
+                if (dt.Rows.Count > 0)
+                {
+                    DgvAsistencia.DataSource = dt;
+                }
+                else
+                {
+                    MessageBox.Show("No se encontraron registros de asistencia.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void FormAsistencia_Load(object sender, EventArgs e)
+        {
+            CmbEquipo.Text = "Seleccione un equipo...";
+        }
+
+        private void CmbEquipo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (CmbEquipo.SelectedValue != null)
+            {
+                int equipoId;
+                if (int.TryParse(CmbEquipo.SelectedValue.ToString(), out equipoId))
+                {
+                    CargarJugadores(equipoId);
+                }
+            }
+        }
+
+        private void CargarJugadores(int equipoId)
+        {
+            Conexion conexion = new Conexion();
+            MySqlConnection conn = conexion.getConexion();
+
+            string query = "SELECT pkidjugador, apellido FROM jugador WHERE fkidequipo = @fkidequipo";
+
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@fkidequipo", equipoId);
+
+                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+
+                CmbJugador.DataSource = dt;
+                CmbJugador.DisplayMember = "apellido";
+                CmbJugador.ValueMember = "pkidjugador";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void BtnIngresar_Click(object sender, EventArgs e)
+        {
+            string minuto = TxtMinuto.Text;
+            int idJugador;
+
+            if (!int.TryParse(CmbJugador.SelectedValue.ToString(), out idJugador))
+            {
+                MessageBox.Show("Jugador no seleccionado.");
+                return;
+            }
+
+            Conexion conexion = new Conexion();
+            MySqlConnection conn = conexion.getConexion();
+
+            string query = "INSERT INTO asistencia (minuto, fkidjugador) VALUES (@minuto, @fkidjugador)";
+
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@minuto", minuto);
+                cmd.Parameters.AddWithValue("@fkidjugador", idJugador);
+
+                int result = cmd.ExecuteNonQuery();
+
+                if (result > 0)
+                {
+                    MessageBox.Show("Asistencia registrada exitosamente.");
+                    TxtIdAsistencia.Clear();
+                    TxtMinuto.Clear();
+                    CmbEquipo.Text = "Seleccione un equipo...";
+                    CmbJugador.Text = "Seleccione un jugador...";
+                }
+                else
+                {
+                    MessageBox.Show("Error al registrar la asistencia.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void BtnModificar_Click(object sender, EventArgs e)
+        {
+            string minuto = TxtMinuto.Text;
+            int idAsistencia, idJugador;
+
+            if (!int.TryParse(TxtIdAsistencia.Text, out idAsistencia))
+            {
+                MessageBox.Show("ID de asistencia inválido.");
+                return;
+            }
+
+            if (!int.TryParse(CmbJugador.SelectedValue.ToString(), out idJugador))
+            {
+                MessageBox.Show("Jugador no seleccionado.");
+                return;
+            }
+
+            Conexion conexion = new Conexion();
+            MySqlConnection conn = conexion.getConexion();
+
+            string query = "UPDATE asistencia SET minuto = @minuto, fkidjugador = @fkidjugador WHERE pkidasistencia = @pkidasistencia";
+
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@minuto", minuto);
+                cmd.Parameters.AddWithValue("@fkidjugador", idJugador);
+                cmd.Parameters.AddWithValue("@pkidasistencia", idAsistencia);
+
+                int result = cmd.ExecuteNonQuery();
+
+                if (result > 0)
+                {
+                    MessageBox.Show("Asistencia modificada exitosamente.");
+                    TxtIdAsistencia.Clear();
+                    TxtMinuto.Clear();
+                    CmbEquipo.Text = "Seleccione un equipo...";
+                    CmbJugador.Text = "Seleccione un jugador...";
+                }
+                else
+                {
+                    MessageBox.Show("Error al modificar la asistencia.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    
+
+
+        private void BtnEliminar_Click(object sender, EventArgs e)
+        {
+            int asistenciaId;
+            if (!int.TryParse(TxtIdAsistencia.Text, out asistenciaId))
+            {
+                MessageBox.Show("ID inválido.");
+                return;
+            }
+
+            Conexion conexion = new Conexion();
+            MySqlConnection conn = conexion.getConexion();
+
+            string query = "DELETE FROM asistencia WHERE pkidasistencia = @pkidasistencia";
+
+            try
+            {
+                MySqlCommand command = new MySqlCommand(query, conn);
+                command.Parameters.AddWithValue("@pkidasistencia", asistenciaId);
+
+                int result = command.ExecuteNonQuery();
+
+                if (result > 0)
+                {
+                    MessageBox.Show("Asistencia eliminada exitosamente.");
+                    TxtIdAsistencia.Clear();
+                    TxtMinuto.Clear();
+                    CmbEquipo.Text = "Seleccione un equipo...";
+                    CmbJugador.Text = "Seleccione un jugador...";
+                }
+                else
+                {
+                    MessageBox.Show("Error al eliminar la asistencia. Verifique el ID.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void TxtIdAsistencia_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DgvAsistencia_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
